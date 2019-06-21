6075fb20bb3f4f0efa07cf3999efb5b33bdba1aa
Leonardo <srleonardofurtado@gmail.com>
2019-06-20 23:25:10 -0300

    tests added

--- test/retangulo_test.py
+++ test/retangulo_test.py
@@ -6,3 +6,12 @@ class TestRetangulo(TestCase):
     def setUp(self):
         TestCase.setUp(self)
         self.fig = Retangulo()
+
+    def test_get_area(self):
+        self.fig.lado_a = 5
+        self.fig.lado_b = 6
+        self.assertEqual(self.fig.get_area(), 30)
+    def test_get_perimetro(self):
+        self.fig.lado_a = 5
+        self.fig.lado_b = 6
+        self.assertEqual(self.fig.get_perimetro(), 22)
