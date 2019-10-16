91eaead9dac03b951dcde417a8c29382b22dcd8b
Leonardo Furtado <srleonardofurtado@gmail.com>
2019-10-16 13:22:29 -0300

    uodate TestQuadrado

--- test/quadrado_test.py
+++ test/quadrado_test.py
@@ -10,11 +10,9 @@ class TestQuadrado(TestCase):
         self.fig = Quadrado()
 
     def test_get_area(self):
-        self.fig.lado_a = 4
-        self.fig.lado_b = 4
+        self.fig.lado = 4
         self.assertEqual(self.fig.get_area(), 16)
 
     def test_get_perimetro(self):
-        self.fig.lado_a = 5
-        self.fig.lado_b = 5
-        self.assertEqual(self.fig.get_area(), 20)
+        self.fig.lado = 5
+        self.assertEqual(self.fig.get_perimetro(), 20)
